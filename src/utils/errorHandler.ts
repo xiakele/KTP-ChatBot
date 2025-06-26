a440c9c361326ab54531f2d8b6f4d3864915a215
xiakele <xiakele123@gmail.com>
2025-06-27 02:39:59 +0800

    fix: improve error handling

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -4,5 +4,15 @@ import { replyWithReply } from "./contextUtilities.js";
 export async function errorHandler(error: BotError): Promise<void> {
   const context = error.ctx;
   console.error("An error occurred:", error.message);
-  await replyWithReply(context, "An error occurred.");
+  try {
+    await replyWithReply(context, "An error occurred.");
+  } catch {
+    try {
+      await context.reply("An error occurred.");
+    } catch {
+      console.log(
+        "Failed to notify error to user. Check internet connections."
+      );
+    }
+  }
 }
